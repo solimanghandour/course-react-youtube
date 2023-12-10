698e8f8bc8ac20cb80768c61fb45c9bf5543956f
solimanghandour <[200~solimanghandour21@gmail.com>
2023-12-10 14:03:13 +0200

    upload

---
+++ src/comp/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import   './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="ali">
+      Designed and developed by Courses4Arab.com
+      <span>🧡</span>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home";
+import HTML from "./pages/html";
+import Css from "./pages/css";
+import Javascript from "./pages/javascript";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <h1>SORROY.........</h1>,
+  },
+
+  {
+    path: "/html",
+    element: <HTML />,
+  },
+
+  {
+    path: "/css",
+    element: <Css />,
+  },
+  {
+    path: "/javascript",
+    element: <Javascript />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+
+    <RouterProvider router={router} />
+
+);
+serviceWorkerRegistration.register();
